add8a1d6cb844e3bb4928ee9cde1fb52d9208d32
adeha047 <dehaven7777@gmail.com>
2021-04-22 09:49:00 -0400

    added new dependencies

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,15 +3,10 @@ const router = require('express').Router();
 var path = require("path");
 
     router.get("/exercise",function (req,res){   
-        res.sendFile(path.join(__dirname,"../public/exercise.html"));
+        res.sendFile(path.join(__dirname,"../public/map.html"));
     });
   
     router.get("/",function(req,res){    
         res.sendFile(path.join(__dirname,"../public/index.html"));
     });
-  
-    router.get("/stats",function(req,res){   
-        res.sendFile(path.join(__dirname,"../public/stats.html"));
-    });
-
     module.exports = router;
